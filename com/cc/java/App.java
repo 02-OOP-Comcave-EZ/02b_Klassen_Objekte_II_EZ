9687a51c9c56ee84abe5ce0d39c4928377794e26
Esther <esther.zirkler@gmail.com>
2024-05-23 10:28:20 +0200

    02 / Felder  + Wertzuweisung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,19 +7,37 @@ public class App {
         // Cat --> Referenztyp (Klasse aus dem das Objekt ensteht)
         // cat1 --> Refenzvariable (Adresse des Objekts im RAM)
 
+        // Objekt erzeugen
         Cat cat1 = new Cat(); // Instanziierung >> Objekt
         output("Blick von aussen: " + cat1); // Adresse des Objekts
         output("Blick von innen: " + cat1.getInstanceVar());
 
+        // Werte zuweisen
+        cat1.name = "Grizabella";
+        cat1.furColor = "white";
+        cat1.age = 29;
+
+        // Werte ausgeben
         output(cat1.name);
         output(cat1.furColor);
-        output(cat1.age);
+        output(Integer.toString(cat1.age)); // Typkonvertierung
+
 
     //     output("--------------------");
         
-    //     Cat cat2 = new Cat();
-    //     output("Blick von aussen: " + cat2);
-    //     output("Blick von innen: " + cat2.getInstanceVar());
+    Cat cat2 = new Cat();
+    output("Blick von aussen: " + cat2);
+    output("Blick von innen: " + cat2.getInstanceVar());
+
+    // Werte zuweisen
+    cat2.name = "Alonso";
+    cat2.furColor = "grey";
+    cat1.age = 35;
+
+    // Werte ausgeben
+    output(cat2.name);
+    output(cat2.furColor);
+    output(Integer.toString(cat2.age)); // Typkonvertierung
 
     }
 
